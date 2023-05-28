bb98d5aadb2f35d4590f99dc72473f992c8f200a
hussam <ooooo.ooooo.hosam@gmail.com>
2023-05-28 17:50:25 +0300

    first commit

--- authenticateToken/authenticateToken.js
+++ authenticateToken/authenticateToken.js
@@ -4,7 +4,7 @@ const authenticateToken = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1]; // Extract the token from the Authorization header
   
   if (!token) {
-    return res.status(401).json({ message: 'Missing token' });
+    return res.status(400).json({ message: 'Missing token' });
   }
   
   try {
